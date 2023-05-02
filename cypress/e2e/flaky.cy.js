94862259daf6465d12e58d9c078914b198d2bbe7
jhonlink93 <jhonatan.ortega.joaqui@correounivalle.edu.co>
2023-05-02 12:35:47 -0500

    prevencion de flaky test

---
+++ cypress/e2e/flaky.cy.js
@@ -0,0 +1,14 @@
+describe('flaky test', () => {
+    it('single query command', () => {
+        cy.visit('/')
+        //reintenta el selector por 4000ms ho hasta que el elemento con el selector y el texto aparezca
+        cy.contains('#root > div.container > div:nth-child(1) > div:nth-child(1) > div > center > div.card-header > h1', 'Bulbasaur')
+        //si lo hacemos por aparte primero obtiene el elemento sin saber si es elemento correcto:
+        //cy.get('#root > div.container > div:nth-child(1) > div:nth-child(1) > div > center > div.card-header > h1').contains('Bulbasaaur')
+
+    })
+    it.skip('alternar comando con aserciones', () => {
+        //al alternar una asercion con un comando cypress reintenta el comando default 4000ms 
+        cy.get('#submit').should('not.be.disabled').click()
+    })
+})
