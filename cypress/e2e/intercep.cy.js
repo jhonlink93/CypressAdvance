8daf60d8f76eaf53de8d503a68fb591ca73cc3a3
Jhonatan <jhonatan.ortega.joaqui@correounivalle.edu.co>
2023-05-08 13:22:56 -0500

    interceptando request

---
+++ cypress/e2e/intercep.cy.js
@@ -0,0 +1,30 @@
+describe('test con intercept', () => {
+    it('prueba intercept', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto')
+            .then((response) => {
+                expect(response.status).to.have.eq(200)
+                expect(response.body).to.have.property('name', 'ditto')
+                cy.log(response.body)
+            });
+    });
+    it('prueba intercept simple', () => {
+        cy.intercept(
+            'GET',
+            'https://pokeapi.co/api/v2/pokemon-species/1'
+        ).as('bulbasaur');
+        cy.visit('/');
+        cy.contains('Bulbasaur')
+            .parent()
+            .parent()
+            .within((element) => {
+                cy.wrap(element).contains('Más detalles').click()
+            })
+        cy.wait('@bulbasaur').then((interception) => {
+            cy.log(interception)
+            expect(interception.response.body).to.have.property('name', 'bulbasaur')
+            expect(interception.response.statusCode).eq(200)
+        })
+        //cy.wait('@bulbasaur').its('response.statusCode').should('eq', 200)
+    });
+
+})
