90e1f9bc2ed4f6cec6fee177a3e93166628f5b7f
Jhonatan <jhonatan.ortega.joaqui@correounivalle.edu.co>
2023-05-29 17:01:44 -0500

    fixing typo

--- cypress/e2e/flaky.cy.js
+++ cypress/e2e/flaky.cy.js
@@ -1,7 +1,7 @@
 describe('flaky test', () => {
     it('single query command', () => {
         cy.visit('/')
-        //reintenta el selector por 4000ms ho hasta que el elemento con el selector y el texto aparezca
+        //reintenta el selector por 4000ms o hasta que el elemento con el selector y el texto aparezca
         cy.contains('#root > div.container > div:nth-child(1) > div:nth-child(1) > div > center > div.card-header > h1', 'Bulbasaur')
         //si lo hacemos por aparte primero obtiene el elemento sin saber si es elemento correcto:
         //cy.get('#root > div.container > div:nth-child(1) > div:nth-child(1) > div > center > div.card-header > h1').contains('Bulbasaaur')
